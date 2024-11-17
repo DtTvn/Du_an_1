e82db74441d8c1ae454b997dcbf10dd4339b0472
Thang-day <thangtmph49748@gmail.com>
2024-11-17 13:22:10 +0700

    thang

--- env.php
+++
@@ -1,9 +0,0 @@
-<?php
-// lấy thư mục gốc của file
-const ROOT_DIR = __DIR__ . "/";
-// Lấy đường dẫn website
-const ROOT_URL = "http://localhost/Du_an_1/view/home.php";
-// Lấy đường dẫn admin 
-const ADMIN_URL = "http://localhost/Du_an_1/view/admin/header.php";
-
-?>
