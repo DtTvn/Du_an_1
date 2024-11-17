119fecfd96d25039730b2d4862ff4d59a53db39d
Thang-day <thangtmph49748@gmail.com>
2024-11-17 23:57:26 +0700

    thang đẩy giao diện admin và chia luồng

--- view/client/home.php
+++ view/client/home.php
@@ -1,51 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang chủ</title>
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    <link rel="stylesheet" href="css/home.css">
-</head>
-
-<body>
-    <header class="header">
-        <div class="header-top">
-            <h4 class="phone">
-                <i class="fa-solid fa-phone"></i>
-                0123 456 789
-            </h4>
-            <div class="menu">
-                <h4>Giới thiệu</h4>
-                <h4>Khuyến mãi</h4>
-                <h4>Giảm giá đặc biệt</h4>
-            </div>
-            <div class="cart">
-                <i class="fa-solid fa-cart-shopping"></i>
-                <i class="fa-regular fa-user"></i>
-            </div>
-        </div>
-        <div class="header-middle">
-            <img src="img/imgda1/logo.jpg" alt="Logo" class="logo">
-            <ul class="nav-links">
-                <li><a href="#">Sản Phẩm</a></li>
-                <li><a href="#">Phòng</a></li>
-                <li><a href="#">Bộ sưu tập</a></li>
-                <li><a href="#">Thiết kế nội thất</a></li>
-                <li><a href="#">Cửa hàng 360 độ</a></li>
-                <li><a href="#">Góc cảm hứng</a></li>
-            </ul>
-            <div class="search">
-                <button class="button">Tìm kiếm sản phẩm<i
-                        class="fa-solid fa-magnifying-glass"></i></button>
-            </div>
-        </div>
-        <div class="banner">
-            <img src="img/imgda1//banner.jpg" alt="Banner">
-        </div>
-    </header>
+<?php
+    include 'header.php';
+?>
     <main>
         <div class="product-list">
             <div class="product-item-wrapper1">
@@ -116,7 +71,7 @@
                     <h4 class="product-name">Bàn nước Orientale walnu</h4>
                     <h4 class="product-price">49,900,000₫</h4>
                     <button class="add-to-cart">THÊM VÀO GIỎ</button>
-                    <button class="view-details">XEM THÊM</button>
+                    <a href="" class="view-details">XEM THÊM</a>
                 </div>
                 <div class="product-card">
                     <img src="img/imgda1//img4.1.jpg" alt="Bàn nước Orientale walnu">
@@ -172,47 +127,6 @@
             </div>
         </div>
     </main>
-    <footer class="footer">
-        <div class="footer1">
-            <div class="footer-connect">
-                <h3>KẾT NỐI VỚI NHÀ XINH</h3>
-                <img src="../images/imgda1/logo2.jpg" alt="Logo" class="logo">
-                <h4>FOLLOW US</h4>
-                <h5>Instagram-Youtube-Facebook</h5>
-                <button class="button">HỆ THỐNG CỬA HÀNG</button>
-            </div>
-            <div class="footer-about">
-                <h3>NHÀ XINH</h3>
-                <h5>Giới thiệu</h5>
-                <h5>Chuyện nhà xinh</h5>
-                <h5>Tổng công ty</h5>
-                <h5>Tuyển dụng</h5>
-                <h5>Đổi trả hàng</h5>
-            </div>
-            <div class="footer-inspiration">
-                <h3>CẢM HỨNG #NHAXINH</h3>
-                <h5>Sản phẩm</h5>
-                <h5>Ý tưởng và cảm hứng</h5>
-            </div>
-            <div class="footer-newsletter">
-                <h3>NEWSLETTER</h3>
-                <h5>Hãy để lại email của bạn để nhận được <br> những ý tưởng
-                    trang
-                    trí mới và những <br> thông tin, ưu đãi từ Nhà Xinh</h5>
-                <h5>Email: nhaxinhcare@akacompany.com.vn</h5>
-                <h5>Hotline: 0111111111</h5>
-            </div>
-        </div>
-        <div class="footer2">
-            <hr>
-            <h5>© 2021 - Bản quyền của Nhà Xinh - thương hiệu thuộc AKA
-                Furniture
-                <br>Từ năm 1999 - thương hiệu đăng ký số 284074 Cục sở hữu
-                trí
-                tuệ
-            </h5>
-        </div>
-    </footer>
-</body>
-
-</html>
+<?php
+    include 'footer.php'
+?>
