b01058652ef60d958d6c781e6bf9c523a94fbc6b
Thang-day <thangtmph49748@gmail.com>
2024-11-21 00:02:33 +0700

    thang

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,6 @@
 <?php 
+    require_once __DIR__ . "/../env.php";
+    require_once __DIR__ . "/../common/function.php";
+    
+    view("admin.dashboard");
 ?>
---
+++ views/admin/products/list.php
@@ -0,0 +1,11 @@
+<?php
+    include_once ROOT_DIR . "./views/admin/header.php"
+?>
+
+<div>
+    
+</div>
+
+<?php 
+    include_once ROOT_DIR . "./views/admin/siderbar.php"
+?>
