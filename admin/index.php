5eff75fbc4cfbbad27677ddeaad3e30b9a8713dc
THANH THONG <thongdo0208305@gmail.com>
2024-11-22 12:33:38 +0700

    danh muc

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
     require_once __DIR__ . "/../env.php";
     require_once __DIR__ . "/../common/function.php";
 
@@ -8,10 +9,15 @@
     require_once __DIR__ . "/../models/Product.php";
     //include controller
     require_once __DIR__ . "/../controllers/admin/AdminProductController.php";
-
+    require_once __DIR__ . "/../controllers/admin/AdminCategoryController.php";
     $ctl = $_GET['ctl'] ?? '';
     match ($ctl) {
         ''  => view("admin.dashboard"),
         'listsp' => (new AdminProductController)->index(),
+        'listdm' => (new AdminCategoryController)->index(),
+        'adddm' => (new AdminCategoryController)->create(),
+        'storedm' => (new AdminCategoryController)->store(),
+        'editdm' => (new AdminCategoryController)->edit(),
+        'updatedm' => (new AdminCategoryController)->update(),
         default => view("errors.404"),
     };
