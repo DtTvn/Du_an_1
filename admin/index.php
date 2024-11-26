3f183ab607b31199543c844bc1ea7dc9273499fe
THANH THONG <thongdo0208305@gmail.com>
2024-11-26 17:10:01 +0700

    code cu

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 session_start();
-    require_once __DIR__ . "/../env.php";
-    require_once __DIR__ . "/../common/function.php";
+require_once __DIR__ . "/../env.php";
+require_once __DIR__ . "/../common/function.php";
 
-    // include models
-    require_once __DIR__ . "/../models/BaseModel.php";
-    require_once __DIR__ . "/../models/Category.php";
-    require_once __DIR__ . "/../models/Product.php";
+// include models
+require_once __DIR__ . "/../models/BaseModel.php";
+require_once __DIR__ . "/../models/Category.php";
+require_once __DIR__ . "/../models/Product.php";
 
-    //include controller
-    require_once __DIR__ . "/../controllers/admin/DashboardController.php";
-    require_once __DIR__ . "/../controllers/admin/AdminProductController.php";
-    require_once __DIR__ . "/../controllers/admin/AdminCategoryController.php";
-    
-    $ctl = $_GET['ctl'] ?? '';
-    match ($ctl) {
+//include controller
+require_once __DIR__ . "/../controllers/admin/DashboardController.php";
+require_once __DIR__ . "/../controllers/admin/AdminProductController.php";
+require_once __DIR__ . "/../controllers/admin/AdminCategoryController.php";
+
+$ctl = $_GET['ctl'] ?? '';
+match ($ctl) {
         '' => (new DashboardController)->index(),
         //Danh mục
 
@@ -27,9 +27,9 @@ session_start();
         //Sản Phẩm
         'listsp' => (new AdminProductController)->index(),
         'addsp' => (new AdminProductController)->add(),
-        'storesp'=> (new AdminProductController)->store(),
-        'editsp'=> (new AdminProductController)->edit(),
+        'storesp' => (new AdminProductController)->store(),
+        'editsp' => (new AdminProductController)->edit(),
         'updatesp' => (new AdminProductController)->update(),
         'deletesp' => (new AdminProductController)->delete(),
         default => view("errors.404"),
-    };
+};
