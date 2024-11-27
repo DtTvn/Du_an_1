eb7c52d5fb5ce5571d43796f86af1b21def9ce2b
Thang-day <thangtmph49748@gmail.com>
2024-11-27 13:19:27 +0700

    thang cập nhập css đã xong

--- views/client/products/category.php
+++ views/client/products/category.php
@@ -5,21 +5,21 @@
     <b><?= $title ?></b>
 </div>
 <h1>Sản phẩm</h1>
+<div class="product-grid">
 <?php foreach ($products as $product) : ?>
-    <div class="product-grid">
-        <div class="product-grids">
-            <div class="product-card">
-                <img src="<?= $product['Image'] ?>" alt="Product Image">
-                <h4 class="product-name"><?= $product['ProductName'] ?></h4>
-                <h4 class="product-price">
-                    <?= $product['Price'] ?> VND
-                </h4>
-                <button class="add-to-cart">THÊM VÀO GIỎ</button>
-                <a href="<?= ROOT_URL . '?ctl=detail&id=' . $product['id']?>">
-                    <button class="view-details">XEM THÊM</button>
-                </a>
-            </div>
+    <div class="product-grids">
+        <div class="product-card">
+            <img src="<?= $product['Image'] ?>" alt="Product Image">
+            <h4 class="product-name"><?= $product['ProductName'] ?></h4>
+            <h4 class="product-price">
+                <?= $product['Price'] ?> VND
+            </h4>
+            <button class="add-to-cart">THÊM VÀO GIỎ</button>
+            <a href="<?= ROOT_URL . '?ctl=detail&id=' . $product['id'] ?>">
+                <button class="view-details">XEM THÊM</button>
+            </a>
         </div>
     </div>
 <?php endforeach ?>
+</div>
 <?php include_once ROOT_DIR . "views/client/footer.php" ?>
